27a2fbb2701fec776a277282736a2c9d987d5350
ybnr1130 <muuckk@gmail.com>
2021-12-26 10:35:14 +0900

    First commit

---
+++ RectangleApp/RectangleApp/RectangleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RectangleAppApp.swift
+//  RectangleApp
+//
+//  Created by 薮内紀仁 on 2021/12/26.
+//
+
+import SwiftUI
+
+@main
+struct RectangleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
